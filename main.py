f21d5331e724c20142ab09580b494a73239ee381
Jesse Mazzie <jessegalt@yahoo.com>
2021-11-10 12:54:22 -0500

    parsing tfstate to determine aws instance private dns

--- main.py
+++ main.py
@@ -3,3 +3,19 @@ import json
 #Open yer file
 f = open('test.tfstate')
 
+data = json.load(f)
+
+for i in data['resources']:
+    if i['type'] == "aws_instance":
+        for j in i['instances']:
+            print(j['attributes']['private_dns'])
+    # for j in i['instances']:
+    #     if "tags" in j['attributes']:
+    #         print(j['attributes']['tags'])
+    #     # if j['tags_all']['Managed'] == "Ansible":
+    #     #     print("yaaa")
+    #     # print(i['name'])
+
+#print(data)
+
+f.close()
